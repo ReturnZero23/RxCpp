1c3e3ae09863940dc9ff637722b8b606f760ca0b
Raofei <raofei@sheencity.com>
2017-09-28 12:54:29 +0800

    判断类型的代码的详细解析

--- Rx/v2/src/rxcpp/rx-sources.hpp
+++ Rx/v2/src/rxcpp/rx-sources.hpp
@@ -19,6 +19,26 @@ struct source_base
     typedef T value_type;
     typedef tag_source source_tag;
 };
+
+/// note by returnzer0:
+/// is_XXX() 在RxCpp中大量使用来判断类型
+/// 
+/// template<class T>
+/// class is_source
+/// {
+///     Part1: template<class C>
+///            static typename C::source_tag* check(int);
+///
+///     Part2: template<class C>
+///            static void check(...);
+/// public:
+///     Part3: static const bool value = std::is_convertible<decltype(check<rxu::decay_t<T>>(0)), tag_source*>::value;
+/// };
+/// Part1: 声明一个特化版本的check，当调用check<class T>时返回T::XXX_tag
+/// Part2: 声明一个泛化版本的check，当调用check<class T>时 T 类型没有 T::XXX_tag 时返回 void 。 
+/// Part3: rxu::decay_t<T> 先取出 T 的类型
+///        然后调用 check<class T> 取出 T::XXX_tag ，然后判断 T::XXX_tag 和 类型 T::XXX_tag是否是可转换的。
+
 template<class T>
 class is_source
 {
